d5bbdcb3e2dc5359e6f94856ae722cdf07d357eb
17Divyakshi <divyakshi2006@gmail.com>
2025-01-31 21:46:47 +0530

    Solution to Simple Calculator

--- Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
+++ Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
@@ -1,30 +1,30 @@
 #include <stdio.h>
 #include<math.h>
 int main() {
-    //int a,b;
-    //char c;
-    //scanf("%d%d%")
+    int a,b;
+    char c;
+    scanf("%d%d%c",&a,&b,&c);
     switch()
     {
         case 1:
-        int a,b;
-        char c;
-        scanf("%d%d%c",&a,&b,&c);
+        // int a,b;
+        // char c;
+        // scanf("%d%d%c",&a,&b,&c);
         printf("%d",a,c,b);
         break;
         case 2:
-        scanf("%d%d%c",&a,&b,&c);
+        // scanf("%d%d%c",&a,&b,&c);
         printf("%d%c%d",a,c,b);
         break;
         case 3:
-        scanf("%d%d%c",&a,&b,&c);
+        // scanf("%d%d%c",&a,&b,&c);
         printf("%d%c%d",a,c,b);
         break;
         case 4:
-        scanf("%d%d%c",&a,&b,&c);
+        // scanf("%d%d%c",&a,&b,&c);
         printf("%d%c%d",a,c,b);
         break;
-        defautl:
+        default:
         printf("error");
     }
     return 0;
