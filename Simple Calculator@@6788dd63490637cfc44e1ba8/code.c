5907c2f7ef0330f26b4b70d4e13322ab6cd2ff0a
17Divyakshi <divyakshi2006@gmail.com>
2025-01-31 21:41:51 +0530

    Solution to Simple Calculator

---
+++ Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+int main() {
+    int e;
+    sacnf("%d",&e);
+    switch(e)
+    {
+        case 1:
+        int a,b;
+        char c;
+        scanf("%d%d%c",&a,&b,&c);
+        printf("%d%c%d",a,c,b);
+        break;
+        case 2:
+        scanf("%d%d%c",&a,&b,&c);
+        printf("%d%c%d",a,c,b);
+        break;
+        case 3:
+        scanf("%d%d%c",&a,&b,&c);
+        printf("%d%c%d",a,c,b);
+        break;
+        case 4:
+        scanf("%d%d%c",&a,&b,&c);
+        printf("%d%c%d",a,c,b);
+        break;
+        defautl:
+        printf("");
+    }
+    return 0;
+}
